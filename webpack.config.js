fb5124c7d91cb820a24a0e82112caa31c8c14ca2
Leul Wujira <meraf000@gmail.com>
2023-12-30 13:44:48 +0300

    feat: setup project structure

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
+const path = require('path');
+const outputPath = 'dist';
+const entryPoints = {
+  main: [path.resolve(__dirname, 'src', 'main.ts')],
+  services: path.resolve(__dirname, 'src', 'services.ts'),
+  'auth.content': path.resolve(__dirname, 'src', 'content/auth.content.ts'),
+};
+
+module.exports = {
+  entry: entryPoints,
+  output: {
+    path: path.join(__dirname, outputPath),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|woff|woff2|eot|ttf|svg)$/i,
+        use: 'url-loader?limit=1024',
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: '.', context: 'public' }],
+    }),
+  ],
+};
