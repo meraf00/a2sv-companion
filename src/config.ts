f5d575db3a38cb8c6c379652e8d77643d94b9718
Leul Wujira <meraf000@gmail.com>
2024-01-03 01:35:41 +0300

    feat: migrate to vercel

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   api: {
-    url: 'https://a2sv.pythonanywhere.com/api',
+    url: 'https://a2sv-companion-backend.vercel.app/api',
     // url: 'http://localhost:5000/api',
   },
 };
